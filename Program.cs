a829a94b95905f62a4cef87bbddfa691fd1c1a8f
Alex Lug <ponuch.lug@gmail.com>
2024-01-20 09:41:01 +0300

    пельменная

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine(12);
+var qna = Double.Parse(Console.ReadLine());
+var qt = Double.Parse(Console.ReadLine());
+var qk = Double.Parse(Console.ReadLine());
+var Qc = Double.Parse(Console.ReadLine());
+var t = Double.Parse(Console.ReadLine());
+var at = Double.Parse(Console.ReadLine());
 
-var str = "as";
-Console.WriteLine(str);
+var Qp = Qc / (2 * t);
+var nna = Qp / qna;
+Console.WriteLine(Convert.ToInt32(nna));
+
+// var Qt = at * Qp;
+// var nt = Qt / qt;
+// Console.WriteLine(Convert.ToInt32(nt));
+
+// var Qf = (1 - at) * Qp;
+// var nk = Qf / qk;
+// Console.WriteLine(Convert.ToInt32(nt));
